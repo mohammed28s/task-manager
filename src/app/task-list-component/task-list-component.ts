15ebc15d0bcb6a30790b24d91ca32f426442b2c5
Mohammed Salim <mohammed.23s@outlook.com>
2025-07-31 12:25:33 +0400

    The TaskList component has been created

---
+++ src/app/task-list-component/task-list-component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from '../tasks.service';
+import { Task } from '../task'; 
+
+@Component({
+  selector: 'app-task-list-component',
+  templateUrl: './task-list-component.html',
+  styleUrl: './task-list-component.css'
+})
+
+export class TaskListComponent implements OnInit {
+
+  tasks: Task[] = [];
+
+  constructor(private taskService: TaskService) { } // this is a constractor for the taskService 
+
+  //by { }
+
+  ngOnInit(): void {
+    this.tasks = this.taskService.getTasks();
+  }
+
+  deleteTask(id: number): void {
+    this.taskService.deleteTask(id);
+  }
+
+}
