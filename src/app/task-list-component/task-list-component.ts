14ee968d7dedf4daff536a5f3573158b4cbfe08b
Mohammed Salim <mohammed.23s@outlook.com>
2025-08-02 16:01:39 +0400

    :The task manager has been complete

--- src/app/task-list-component/task-list-component.ts
+++ src/app/task-list-component/task-list-component.ts
@@ -24,6 +24,7 @@ export class TaskListComponent implements OnInit {
 
   deleteTask(id: number): void {
     this.taskService.deleteTask(id);
+    this.tasks = this.taskService.getTasks(); // Refresh the task list after deletion
   }
 
 }
