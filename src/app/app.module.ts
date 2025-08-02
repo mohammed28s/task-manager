e0c29f96e22ef332ac4ce265a75651624670ea58
Mohammed Salim <mohammed.23s@outlook.com>
2025-08-02 15:42:52 +0400

    :The two way biding has been added

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+// import { NgModule } from "@angular/core";
+// import { BrowserModule } from "@angular/platform-browser";  
+// import { FormsModule } from "@angular/forms";
+// import { AppRoutingModule } from "./app-routing.module";
+// import { App } from "./app";
+// import { TaskListComponent } from "./task-list-component/task-list-component";
+// import { AddTaskComponent } from "./add-task-component/add-task-component";
+
+// @NgModule({
+//     declarations: [
+//         App,
+//         TaskListComponent,
+//         AddTaskComponent
+//     ],
+//     imports: [
+//         BrowserModule,
+//         FormsModule,
+//         AppRoutingModule
+//     ],
+//     providers: [],
+//     bootstrap: [AppComponent]
+// })
+
+// export class AppModule { }
+
--- src/app/app.ts
+++ src/app/app.ts
@@ -2,12 +2,15 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AddTaskComponent } from "./add-task-component/add-task-component";
 import { TaskListComponent } from "./task-list-component/task-list-component";
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AddTaskComponent, TaskListComponent],
+  imports: [RouterOutlet, AddTaskComponent, TaskListComponent, FormsModule],
+  standalone: true,
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('task-manager');
