406a89a7cfcb6ae83592b39dc33224d068c72905
Mohammed Salim <mohammed.23s@outlook.com>
2025-08-02 17:15:34 +0400

    :The validation has been added

--- src/app/add-task-component/add-task-component.ts
+++ src/app/add-task-component/add-task-component.ts
@@ -1,22 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TaskService } from '../tasks.service';
-import { FormsModule } from '@angular/forms'; // Import FormsModule for ngModel
-@Component({
-  selector: 'app-add-task-component',
-  imports: [FormsModule],
-  templateUrl: './add-task-component.html',
-  styleUrl: './add-task-component.css'
-})
-export class AddTaskComponent {
-  newTaskTitle: string = ''; // this is for stroing new task title from user 
+import { FormsModule, ReactiveFormsModule, FormGroup, Validators, FormBuilder, AbstractControl } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
-  constructor(private taskService: TaskService) {}
+@Component({
+  standalone: true,
+  selector: 'app-add-task-component',
+  templateUrl: './add-task-component.html',
+  styleUrls: ['./add-task-component.css'],
+  // ✅ Add ReactiveFormsModule
+  imports: [FormsModule, ReactiveFormsModule, CommonModule]
+})
+export class AddTaskComponent implements OnInit {
+
+  newTaskTitle: string = '';
+  taskForm: FormGroup;
+
+  constructor(private taskService: TaskService, private fb: FormBuilder) {
+    this.taskForm = this.fb.group({
+      title: ['', [Validators.required, this.noSpecialCharactersValidator()]]
+    });
+  }
+
+  ngOnInit(): void {}
+
+  noSpecialCharactersValidator() {
+    return (control: AbstractControl) => {
+      const regex = /^[a-zA-Z0-9\s]*$/;
+      return regex.test(control.value) ? null : { specialCharacters: true };
+    };
+  }
 
   addTask(): void {
-    if (this.newTaskTitle.trim()) {
-      this.taskService.addTask(this.newTaskTitle);
-      this.newTaskTitle = '';
+    if (this.taskForm.valid) {
+      const title = this.taskForm.get('title')?.value;
+      this.taskService.addTask(title);
+      this.taskForm.reset();
     }
   }
 
+  get title() {
+    return this.taskForm.get('title');
+  }
 }
--- src/app/app.ts
+++ src/app/app.ts
@@ -2,12 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AddTaskComponent } from "./add-task-component/add-task-component";
 import { TaskListComponent } from "./task-list-component/task-list-component";
-import { FormsModule } from '@angular/forms';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AddTaskComponent, TaskListComponent, FormsModule],
+  imports: [RouterOutlet, AddTaskComponent, TaskListComponent, FormsModule, ReactiveFormsModule],
   standalone: true,
   templateUrl: './app.html',
   styleUrl: './app.css',
